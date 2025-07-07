e24ea25a643c0c99502003434dda89b68eec199d
Farhad Ariyamaram <farhad.ariamaram@gmail.com>
2025-07-07 08:52:33 +0330

    Create Entitites

---
+++ src/CQRS.Domain/Entities/Book.cs
@@ -0,0 +1,31 @@
+﻿namespace CQRS.Domain.Entities;
+
+public class Book
+{
+    public Guid Id {private set;get;}
+    public string Title {private set;get;}
+    public string Author {private set;get;}
+
+    private Book(){}
+
+    public Book(string title, string author)
+    {
+        if(string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title cannot be empty");
+
+        if(string.IsNullOrWhiteSpace(author))
+            throw new ArgumentException("Author cannot be empty");
+
+        Id = Guid.NewGuid();
+        Title = title;
+        Author = author;
+    }
+
+    public void Rename(string newTitle)
+    {
+        if(string.IsNullOrWhiteSpace(newTitle))
+            throw new ArgumentException("Title cannot be empty");
+
+        Title = title;
+    }
+}
