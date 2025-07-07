bdf176fe9b16d5eaad6c125263e8c3c4c86eb8ae
Farhad Ariyamaram <farhad.ariamaram@gmail.com>
2025-07-07 10:00:50 +0330

    Add ApiContorller - Run a command - Change OpenAPI to normal - Add Swagger

---
+++ src/CQRS.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using CQRS.Application.Books.Commands;
+using MediatR;
+
+namespace MyApp.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BooksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddBookCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return Ok(new {BookId = id});
+        }
+    }
+}
